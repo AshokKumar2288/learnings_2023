9647e7664357162f9b24acc455960deeb9e3d268
AshokKumar2288 <134308649+AshokKumar2288@users.noreply.github.com>
2023-06-15 16:53:04 +0530

    Update ex5.c

--- Module1/Day1/ex5.c
+++ Module1/Day1/ex5.c
@@ -1,39 +1,34 @@
 #include <stdio.h>
 
+int bit_operations(int num, int oper_type) {
+    switch (oper_type) {
+        case 1:
+            num |= (1 << 0);    // Set 1st bit
+            break;
+        case 2:
+            num &= ~(1 << 31);   // Clear 31st bit
+            break;
+        case 3:
+            num ^= (1 << 15);    // Toggle 16th bit
+            break;
+        default:
+            printf("Error: Invalid operation type\n");
+            break;
+    }
+    return num;
+}
 int main() {
-  
-    int roll_no;
-    char name[100];
-    float physics_marks, math_marks, chemistry_marks;
-    
-    printf("Enter Roll No: ");
-    scanf("%d", &roll_no);
-    
-    printf("Enter Name: ");
-    scanf("%s", name);
-    
-    printf("Enter Marks in Physics: ");
-    scanf("%f", &physics_marks);
-    
-    printf("Enter Marks in Math: ");
-    scanf("%f", &math_marks);
-    
-    printf("Enter Marks in Chemistry: ");
-    scanf("%f", &chemistry_marks);
-    
-    
-    float total_marks = physics_marks + math_marks + chemistry_marks;
-    float percentage = (total_marks / 300) * 100;
-    
-    
-    printf("\n--- Student Summary ---\n");
-    printf("Roll No: %d\n", roll_no);
-    printf("Name: %s\n", name);
-    printf("Marks in Physics: %.2f\n", physics_marks);
-    printf("Marks in Math: %.2f\n", math_marks);
-    printf("Marks in Chemistry: %.2f\n", chemistry_marks);
-    printf("Total Marks: %.2f\n", total_marks);
-    printf("Percentage: %.2f%%\n", percentage);
-    
+    int num, oper_type, result;
+
+    printf("Enter the integer: ");
+    scanf("%d", &num);
+
+    printf("Enter the operation type (1, 2, or 3): ");
+    scanf("%d", &oper_type);
+
+    result = bit_operations(num, oper_type);
+
+    printf("Result: %d\n", result);
+
     return 0;
 }
