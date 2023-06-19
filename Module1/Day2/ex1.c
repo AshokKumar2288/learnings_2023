c9321b2127bdc993a94d2d22f618c02d9ee75b9d
AshokKumar2288 <134308649+AshokKumar2288@users.noreply.github.com>
2023-06-19 09:15:27 +0530

    Update ex1.c

--- Module1/Day2/ex1.c
+++ Module1/Day2/ex1.c
@@ -1,24 +1,19 @@
-#include<stdio.h>  
-  
-int biggest(int, int); 
-  
-int main()  
-{  
-    int a, b;  
-  
-    printf("Enter 2 integer numbers\n");  
-    scanf("%d%d", &a, &b);  
-  
-    int c= biggest(a, b);
-    printf("Biggest is %d\n",c);  
-  
-    return 0;  
-}  
-  
-int biggest(int x, int y)  
-{  
-    if(x>y)
-    return x;
-    else
-    return y;
-}  
+#include <stdio.h>
+
+int main() {
+    double x = 0.7;
+    unsigned long long *ptr = (unsigned long long *)&x; // Pointer to treat x as unsigned long long
+
+    unsigned long long exponent = (*ptr & 0x7FF0000000000000) >> 52; // Extracting exponent bits
+
+    printf("Exponent in hexadecimal: 0x%llX\n", exponent);
+    
+    printf("Exponent in binary: 0b");
+    for (int i = 11; i >= 0; i--) {
+        unsigned long long bit = (exponent >> i) & 1;
+        printf("%llu", bit);
+    }
+    printf("\n");
+
+    return 0;
+}
